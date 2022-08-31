0278649b21e63f2840ec3f525d7df39f497e42c0
RangeOfGlitching <58881058+RangeOfGlitching@users.noreply.github.com>
2022-08-31 09:00:52 +0800

    Add files via upload

---
+++ src/wifi_config.h
@@ -0,0 +1,15 @@
+#define cordic45
+#ifdef asus
+	#define WIFI_SSID "asus"
+	#define WIFI_KEY "0989212698"
+	#define CLOUD "192.168.50.252"
+#endif
+
+#ifdef cordic45
+	#define WIFI_SSID "cordic45"
+	#define WIFI_KEY "123456789"
+	#define CLOUD "192.168.43.62"
+#endif
+
+#define USER ""
+#define PASS ""
